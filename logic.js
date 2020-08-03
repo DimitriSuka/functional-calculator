9b1a18f5f15fad879c450038963a2b65e73a0492
DimitriSuka <dimitri_suka@protonmail.ch>
2020-08-04 07:32:41 +0930

    Starting the buttons. only HTML for now

---
+++ logic.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(num1, num2) {
+    return multiply(num1, num2);
+}
+
